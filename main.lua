47b57a86c25e4a39459e6ed6730e2845475182d8
5rmsn4tt2c-ux <5rmsn4tt2c@privaterelay.appleid.com>
2026-06-21 20:30:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-local license = ... or {}
-license.Key = script_key or license.Key or nil
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
@@ -24,6 +22,16 @@ end
 local playersService = cloneref(game:GetService('Players'))
 local httpService = cloneref(game:GetService('HttpService'))
 
+if not isfolder('fart') then
+	makefolder('fart')
+end
+if not isfolder('fart/profiles') then
+	makefolder('fart/profiles')
+end
+if not isfile('fart/profiles/commit.txt') then
+	writefile('fart/profiles/commit.txt', 'main')
+end
+
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
@@ -36,6 +44,17 @@ local function downloadFile(path, func)
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
 		end
 		writefile(path, res)
+	else
+		local content = readfile(path)
+		if not content:find('--This watermark') then
+			local suc, res = pcall(function()
+				return game:HttpGet('https://raw.githubusercontent.com/5rmsn4tt2c-ux/fart_/'..readfile('fart/profiles/commit.txt')..'/'..select(1, path:gsub('fart/', '')), true)
+			end)
+			if suc and res ~= '404: Not Found' then
+				res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+				writefile(path, res)
+			end
+		end
 	end
 	return (func or readfile)(path)
 end
@@ -61,11 +80,10 @@ local function finishLoading()
 					loadstring(game:HttpGet('https://raw.githubusercontent.com/5rmsn4tt2c-ux/fart_/main/main.lua'), 'init')(_scriptconfig)
 				end
 			]]
-			local teleportConfig = httpService:JSONEncode(license)
+			local teleportConfig = httpService:JSONEncode({})
 			teleportConfig = teleportConfig:gsub('":true', "=true"):gsub('{"', '{')
 			teleportConfig = teleportConfig:gsub(',"', ','):gsub('":', '=')
 			teleportConfig = teleportConfig:gsub('%[', '{'):gsub('%]', '}')
-			teleportScript = teleportScript:gsub('_key', tostring(license.Key or '_key'))
 			teleportScript = teleportScript:gsub('_scriptconfig', teleportConfig)
 			if shared.VapeDeveloper then
 				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
@@ -80,57 +98,31 @@ local function finishLoading()
 	if not shared.vapereload then
 		if not vape.Categories then return end
 		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			if getgenv().catrole == 'HWID MISMATCH' then
-				vape:CreateNotification('Cat', 'HWID MISMATCH, Go to the script panel to reset hwid', 25, 'alert')
-				getgenv().catrole = ''
-				task.wait(0.1)
-			end
-			vape:CreateNotification('Finished Loading', (getgenv().catname and `Authenticated as {getgenv().catname} with {getgenv().catrole}, ` or '').. (vape.VapeButton and 'Press the button in the top right' or 'Press '..table.concat(vape.Keybind, ' + '):upper())..' to open GUI', 5)
+			vape:CreateNotification('Finished Loading', (vape.VapeButton and 'Press the button in the top right' or 'Press '..table.concat(vape.Keybind, ' + '):upper())..' to open GUI', 5)
 		end
 	end
 end
 
--- Create required folders and files if they don't exist
-if not isfolder('fart') then
-	makefolder('fart')
-end
-if not isfolder('fart/profiles') then
-	makefolder('fart/profiles')
-end
-if not isfile('fart/profiles/commit.txt') then
-	writefile('fart/profiles/commit.txt', 'main')
-end
 if not isfile('fart/profiles/gui.txt') then
 	writefile('fart/profiles/gui.txt', 'new')
 end
-
 local gui = 'new'
 
-if not isfolder('fart/assets') then
-	makefolder('fart/assets')
-end
 if not isfolder('fart/assets/'..gui) then
 	makefolder('fart/assets/'..gui)
 end
-
-vape = loadstring(downloadFile('fart/guis/'..gui..'.lua'), 'gui')(license)
+vape = loadstring(downloadFile('fart/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
 
 if not shared.VapeIndependent then
-	loadstring(downloadFile('fart/games/universal.lua'), 'universal')(license)
-	if isfile('fart/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('fart/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(license)
-	else
-		if not shared.VapeDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/5rmsn4tt2c-ux/fart_/'..readfile('fart/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('fart/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(license)
-			end
-		end
+	loadstring(downloadFile('fart/games/universal.lua'), 'universal')()
+	local suc, err = pcall(function()
+		loadstring(downloadFile('fart/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))()
+	end)
+	if not suc then
+		vape:CreateNotification('Fart', 'Game file failed: '..tostring(err), 10, 'warning')
 	end
-	loadstring(downloadFile('fart/libraries/premium.lua'), 'premium')(license)
+	loadstring(downloadFile('fart/libraries/premium.lua'), 'premium')()
 	finishLoading()
 else
 	vape.Init = finishLoading
